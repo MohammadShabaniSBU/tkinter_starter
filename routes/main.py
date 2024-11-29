0a549a08ff9aa0c6edcea296710c28bc375847b7
Ghazaal <ghazaal.laghaee@gmail.com>
2024-11-29 23:23:56 +0330

    add routes

--- routes/main.py
+++ routes/main.py
@@ -2,6 +2,8 @@ from routes.dragon import create_dragon_routes
 from routes.hat import create_hat_routes
 from routes.door import create_door_routes
 from routes.matchstick import create_matchstick_routes
+from routes.cannibal import create_cannibal_routes
+from routes.exploration import create_exploration_routes
 
 
 def create_routes(router):
@@ -9,3 +11,5 @@ def create_routes(router):
     create_hat_routes(router)
     create_door_routes(router)
     create_matchstick_routes(router)
+    create_cannibal_routes(router)
+    create_exploration_routes(router)
